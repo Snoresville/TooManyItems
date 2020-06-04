75a8aa99c9072eca783196e9bd9052dfddaf7d10
Snoresville <reflectiveflawless666@gmail.com>
2020-06-05 02:51:47 +0930

    functional snapshot

--- modscript.lua
+++ modscript.lua
@@ -2,35 +2,43 @@
 require("config")
 
 --- creates an item pool which contains vanilla items.
-local allItems = ItemPool.new("all items")
--- Spawn Locked Items option
+local allItems = ItemPool.new("TooManyItems_Pool")
+
+-- loads vanilla pools and a function for insertion
+local vanillaPools = {
+	ItemPool.find("common", "vanilla"), -- commons
+	ItemPool.find("uncommon", "vanilla"), -- uncommons
+	ItemPool.find("rare", "vanilla"), -- rares
+	ItemPool.find("use", "vanilla") -- use items
+}
+
+function insertItemPool(itemPool)
+	local itemList = itemPool:toList()
+	for _, item in ipairs(itemList) do
+		if item.color == "or" and SPAWN_USE_ITEMS == false then goto insertItemPoolSkip end -- use item check
+	
+		allItems:add(item)
+		if item.color == "w" then allItems:setWeight(item, ITEM_COMMON_WEIGHT) end
+		if item.color == "g" then allItems:setWeight(item, ITEM_UNCOMMON_WEIGHT) end
+		if item.color == "r" then allItems:setWeight(item, ITEM_RARE_WEIGHT) end
+		if item.color == "or" then allItems:setWeight(item, ITEM_USE_WEIGHT) end
+		
+		::insertItemPoolSkip::
+	end
+end
+
+for _, itemPool in ipairs(vanillaPools) do
+	insertItemPool(itemPool)
+end
+
+-- item pool options
 if SPAWN_LOCKED_ITEMS == false then allItems.ignoreLocks = false 
 else allItems.ignoreLocks = true end
---
+
 allItems.ignoreEnigma = true
 
-local common = ItemPool.find("common"):toList()
-for _, item in ipairs(common) do
-    allItems:add(item)
-	allItems:setWeight(item, ITEM_COMMON_WEIGHT)
-end
-
-local uncommon = ItemPool.find("uncommon"):toList()
-for _, item in ipairs(uncommon) do
-    allItems:add(item)
-	allItems:setWeight(item, ITEM_UNCOMMON_WEIGHT)
-end
-
-local rare = ItemPool.find("rare"):toList()
-for _, item in ipairs(rare) do
-    allItems:add(item)
-	allItems:setWeight(item, ITEM_RARE_WEIGHT)
-end
-
--- Rarity Balance Option
 if ITEM_RARITY_BALANCE == false then allItems.weighted = false 
 else allItems.weighted = true end
---
 
 --- on enemy death, drops an item
 registercallback("onNPCDeathProc", function(npc, players)
